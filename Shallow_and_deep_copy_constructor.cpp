dbb1841e7610bcf5af9db563375ef5463b8819a6
Nafees Shahzad <shahzadnafees58@gmail.com>
2024-12-29 18:56:45 +0500

    wraping up shallow and copy constructor

--- Shallow_and_deep_copy_constructor.cpp
+++ Shallow_and_deep_copy_constructor.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 class Student {
 
-public:
-   string name;
-   double* cgpaptr;
 
-// parameterized constructor
+public:
+
+    string name;
+    double* cgpaptr;
+    // parameterized constructor
     Student(string name, double cgpa) {
         this->name = name;
         cgpaptr = new double;
         *cgpaptr = cgpa;
     }
 
-void getInfo(){
-    cout<<"Name: "<<name<<endl;
-    cout<<"CGPA: "<<*cgpaptr<<endl;
-}
+    // copy constructor
+    Student(const Student &obj) {
+        this->name = obj.name;
+        cgpaptr = new double;
+        *cgpaptr = *(obj.cgpaptr);
+    }
 
+    void getInfo() {
+        cout << "Name: " << name << endl;
+        cout << "CGPA: " << *cgpaptr << endl;
+    }
 
+   
 };
 
 int main() {
-
-Student s1("Ali", 3.4);
-Student s2(s1);
-// s1.getInfo(); // default copy constructor called
-*(s2.cgpaptr) = 9.2;
-s1.getInfo();
-// s2.getInfo();
+    Student s1("Ali", 3.4);
+    Student s2(s1);
+    // s1.getInfo(); // default copy constructor called
+    *(s2.cgpaptr) = 9.2;
+    s1.getInfo();
+    s2.name = "Ahmed";
+    s2.getInfo();
     return 0;
 }
