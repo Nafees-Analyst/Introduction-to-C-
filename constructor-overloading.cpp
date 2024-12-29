c1c4effa750a8aed5f20fdd8b9fd0519bf139063
Nafees Shahzad <shahzadnafees58@gmail.com>
2024-12-29 21:05:50 +0500

    updated constructor overloading

--- constructor-overloading.cpp
+++ constructor-overloading.cpp
@@ -1,24 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 class Student{
-public:
-    string name;
-
-    Student(){
-        cout<<"non parameterized constructor"<<endl;
-    }
-
-    Student(string name){
-        this->name = name;
-        cout<<"parameterized constructor"<<endl;
-    }
+ public: 
+  string name;
+  
+  Student(){
+      cout<<"non parameterized"<<endl;
+  }
+  
+  Student(string name){
+      this->name = name;
+      cout<<"parameterized"<<endl;
+  }
+  
 };
 
+int main() {
+    Student s1("nafees");
 
-int mian(){
-Student s1("Ali");
-
-
-return 0;
-};
+    return 0;
+}
